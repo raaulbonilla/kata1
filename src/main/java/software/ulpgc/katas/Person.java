6fc43d5aad0a89790e9f17dac62d5e372953e972
raaulbonilla <rbonillacaceres2@gmail.com>
2026-09-23 09:21:08 +0100

    Peron as record

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,13 +1,4 @@
 package software.ulpgc.katas;
 
-public class Person {
-    private final String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public String name() {
-        return name;
-    }
+public record Person(String name) {
 }
