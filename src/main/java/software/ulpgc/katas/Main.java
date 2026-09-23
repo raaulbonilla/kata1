1ef8d3eab4c221bf842cdf1fff2252b2a0e91693
raaulbonilla <rbonillacaceres2@gmail.com>
2026-09-23 09:36:19 +0100

    Main updated

--- src/main/java/software/ulpgc/katas/Main.java
+++ src/main/java/software/ulpgc/katas/Main.java
@@ -3,6 +3,7 @@ package software.ulpgc.katas;
 import java.time.LocalDate;
 
 public class Main {
+
     static void main() {
         Person person = new Person("Lúcia", LocalDate.of(2005, 3, 2));
         System.out.println(person.age());
