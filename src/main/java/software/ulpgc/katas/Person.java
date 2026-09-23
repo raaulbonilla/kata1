e62f608cdf605b37887fc8c01060ab892e4b1c42
raaulbonilla <rbonillacaceres2@gmail.com>
2026-09-23 09:30:58 +0100

    Person with age attribute

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -3,4 +3,14 @@ package software.ulpgc.katas;
 import java.time.LocalDate;
 
 public record Person(String name, LocalDate birthday) {
+
+
+    public int age() {
+        return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private static final double DAYS_PER_YEAR = 365.25;
+    private int toYears(long days) {
+        return (int) (days/ DAYS_PER_YEAR);
+    }
 }
